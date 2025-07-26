f194c360750da87b3d6a4e4b34cdfe78c733db9f
Byongho <unlike96@gmail.com>
2025-07-26 21:59:53 +0900

    refactor

--- gui/ui/organisms/decrypt_image.py
+++ gui/ui/organisms/decrypt_image.py
@@ -1,6 +1,7 @@
 import io
 import customtkinter as ctk
 import tkinter.filedialog as fd
+import torch
 from ui.atoms.toast import Toast
 from ui.atoms.button import Button
 from ui.atoms.input import Input
@@ -25,7 +26,8 @@ class DecryptImage(ctk.CTkFrame):
 
         buffer = io.BytesIO()
         self.latent_image_label.original_image.save(buffer, format="PNG")
-        size_kb = len(buffer.getvalue()) / 1024  # size in KB
+        # size_kb = len(buffer.getvalue()) / 1024  # size in KB
+        size_kb = message["enc_latent_size"] / 1024  # size in KB
         self.size_label = ctk.CTkLabel(self, text=f"Size: {size_kb:.0f} KB", font=("Helvetica", 16), text_color="gray")
         self.size_label.pack(pady=(0, 10))
 
@@ -71,10 +73,11 @@ class DecryptImage(ctk.CTkFrame):
 
     def save_image(self):
         try:
-            decoded_image = self.latent_image_label.original_image
-            file_path = fd.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")], initialfile="decrypted_image.png")
+            file_path = fd.asksaveasfilename(    defaultextension=".pt",
+                filetypes=[("PyTorch Tensor", "*.pt"), ("All Files", "*.*")],
+                title="Save Tensor As"
+            )
             if file_path:
-                decoded_image.save(file_path)
-                Toast(self, "Image saved successfully!", type="success", duration=2000)
+                torch.save(self.message['enc_latent_tensor'], file_path)
         except Exception as e:
             Toast(self, f"Error saving image: {str(e)}", type="error", duration=2000)
